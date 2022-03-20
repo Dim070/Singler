865f0a7978b4fb7bd4b2c4d28e30a3e5840a2cdc
Dim070 <afer93@gmail.com>
2022-03-20 21:55:33 +0700

    +border

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 let offset = 0
 const sliderLine = document.querySelector('.slider_line')
+const arRight = document.querySelector('.arrow_right')
+const arLeft = document.querySelector('.arrow_left')
+const lastSlider = document.querySelector('.last-card')
+const cards = document.querySelectorAll('.card')
 
-document.querySelector('.arrow_right').addEventListener('click', () => {
-   console.log('sss');
+arRight.addEventListener('click', () => {
    offset = offset + 19.79
    if (offset > 57.81) {
       offset = 0
@@ -10,10 +13,21 @@ document.querySelector('.arrow_right').addEventListener('click', () => {
    sliderLine.style.left = -offset + 'vw'
 })
 
-document.querySelector('.arrow_left').addEventListener('click', () => {
+arLeft.addEventListener('click', () => {
    offset = offset - 19.79
    if (offset < 0) {
       offset = 39.58
    }
    sliderLine.style.left = -offset + 'vw'
+})
+
+
+
+arRight.addEventListener('click', () => {
+   console.log('1');
+   //cards.forEach(el => el.classList.toggle('last-card'));
+   for (let i = 2; i < cards.length; i++) {
+
+      console.log(i)
+   }
 })
